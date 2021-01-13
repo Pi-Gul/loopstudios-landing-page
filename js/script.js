995e469df021a21b512ca71353cc265b50b03111
Pi-Gul <photon-drive@protonmail.com>
2021-01-13 17:16:34 +0100

    Add highlights; fix problems with the nav menu; apply stylistic corrections

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const menuButton = document.querySelector('.hamburger-icon-img');
+const hamburgerButton = document.querySelector('.hamburger-icon-img');
+const closeButton = document.querySelector('.close-icon-img');
 const mainNav = document.querySelector('.main-nav');
 const bannerH1 = document.getElementById('banner-h1');
+const mediaQuery = window.matchMedia('(min-width: 1100px)');
 
-bannerH1.innerHTML = 'TEST';
+//bannerH1.innerHTML = 'TEST';
 
-function toggleMenu(ev) {
+/*function toggleMenu(ev) {
     bannerH1.innerHTML = 'IT WORKS';
     let mainNavStyle = window.getComputedStyle(mainNav);
     if(mainNavStyle.getPropertyValue('display')==='none') {
@@ -14,11 +16,32 @@ function toggleMenu(ev) {
         mainNav.style.setProperty('display', 'none');
         menuButton.setAttribute('src', './images/icon-hamburger.svg');
     };
-};
+};*/
+
+function toggleMenu(ev) {
+    let mainNavStyle = window.getComputedStyle(mainNav);
+    if(mainNavStyle.getPropertyValue('display')==='none') {
+        mainNav.style.setProperty('display', 'block');
+        hamburgerButton.setAttribute('src', './images/icon-close.svg');
+    } else if(mainNavStyle.getPropertyValue('display')==='block') {
+        mainNav.style.setProperty('display', 'none');
+        hamburgerButton.setAttribute('src', './images/icon-hamburger.svg');
+    };
+}
 
 function activateMenu(ev) {
-    mainNav.style.display = 'block';
+    if(ev.matches) {
+        mainNav.style.display = 'block';
+        console.log('ACTIVATE');
+    } else {
+        mainNav.style.display = 'none';
+        hamburgerButton.setAttribute('src', './images/icon-hamburger.svg');
+        console.log('DEACTIVATE')
+    }
+    
 };
 
-menuButton.addEventListener('click', toggleMenu);
-window.matchMedia('only screen and (min-width: 1100px)').onchange = activateMenu;
+hamburgerButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+
+mediaQuery.addListener(activateMenu);
